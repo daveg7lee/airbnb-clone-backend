11f75a13f71119d05cd60a82e2159e46464873a4
daveg7lee <daveg7lee@gmail.com>
2022-09-24 20:24:36 +0900

    JsonResponse

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Category
 
-# Create your views here.
+
+def categories(request):
+    categories = Category.objects.all()
+
+    return JsonResponse(
+        {"success": True, "categories": categories},
+    )
