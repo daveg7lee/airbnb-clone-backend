a04fa90d3e723a60ee4682e7cbeca96127fe8f4e
daveg7lee <daveg7lee@gmail.com>
2022-09-24 16:00:38 +0900

    Custom Filters

--- reviews/admin.py
+++ reviews/admin.py
@@ -2,9 +2,33 @@ from django.contrib import admin
 from .models import Review
 
 
+class RatingFilter(admin.SimpleListFilter):
+
+    parameter_name = "input_rating"
+    title = "Filter by rating"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("good", "Good"),
+            ("bad", "Bad"),
+        ]
+
+    def queryset(self, request, reviews):
+        rating = request.GET.get("input_rating")
+        if rating == "good":
+            return reviews.filter(rating__gte=3)
+        elif rating == "bad":
+            return reviews.filter(rating__lt=3)
+        else:
+            return reviews
+
+
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
 
     list_display = ("__str__", "payload")
 
-    list_filter = ("rating",)
+    list_filter = (
+        "rating",
+        RatingFilter,
+    )
