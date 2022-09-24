b80c7817d895fe2ca6c0493c59af01cad95efc33
daveg7lee <daveg7lee@gmail.com>
2022-09-24 19:48:18 +0900

    render

--- rooms/views.py
+++ rooms/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 
 
 def see_all_rooms(request):
-    return HttpResponse("Hello!")
+    rooms = Room.objects.all()
+
+    return render(request, "all_rooms.html", {"rooms": rooms, "title": "Hello!"})
 
 
 def see_one_room(request, pk):
