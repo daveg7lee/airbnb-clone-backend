fb4231d2d55880b6d9e4adeda410298fbbd3853e
daveg7lee <daveg7lee@gmail.com>
2022-09-30 16:39:43 +0900

    Create a Booking

--- rooms/views.py
+++ rooms/views.py
@@ -10,7 +10,7 @@ from categories.models import Category
 from reviews.serializers import ReviewSerializer
 from medias.serializers import PhotoSerializer
 from bookings.models import Booking
-from bookings.serializers import PublicBookingSerializer
+from bookings.serializers import PublicBookingSerializer, CreateRoomBookingSerializer
 from .models import Amenity, Room
 from .serializers import AmenitySerializer, RoomListSerializer, RoomDetailSerializer
 
@@ -303,3 +303,11 @@ class RoomBookings(APIView):
         )
 
         return Response(serializer.data)
+
+    def post(self, request, pk):
+        room = self.get_object(pk)
+        serializer = CreateRoomBookingSerializer(data=request.data)
+        if serializer.is_valid():
+            pass
+        else:
+            return Response(serializer.errors)
