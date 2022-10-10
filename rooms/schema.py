41a19924f6a8dc91bd15e987b40fb61823aa6805
daveg7lee <daveg7lee@gmail.com>
2022-10-10 23:17:27 +0900

    Code challenge

--- rooms/schema.py
+++ rooms/schema.py
@@ -2,12 +2,18 @@ import strawberry
 import typing
 from . import types
 from . import quries
+from . import mutations
 from common.permissions import OnlyLoggedIn
 
 
 @strawberry.type
 class Query:
-    all_rooms: typing.List[types.RoomType] = strawberry.field(
-        resolver=quries.get_all_rooms, permission_classes=[OnlyLoggedIn]
-    )
+    all_rooms: typing.List[types.RoomType] = strawberry.field(resolver=quries.get_all_rooms)
     room: typing.Optional[types.RoomType] = strawberry.field(resolver=quries.get_room)
+
+
+@strawberry.type
+class Mutation:
+    room: typing.Optional[types.RoomType] = strawberry.mutation(
+        resolver=mutations.add_room, permission_classes=[OnlyLoggedIn]
+    )
