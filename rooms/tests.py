3caccb53a169b004c203eed0081d52276d8cbcc7
daveg7lee <daveg7lee@gmail.com>
2022-10-14 18:30:41 +0900

    Create Amenity Test

--- rooms/tests.py
+++ rooms/tests.py
@@ -6,13 +6,14 @@ class TestAmenities(APITestCase):
 
     NAME = "Amenity test"
     DESCRIPTION = "Amenity test"
+    URL = "/api/v1/rooms/amenities/"
 
     def setUp(self) -> None:
         models.Amenity.objects.create(name=self.NAME, description=self.DESCRIPTION)
 
     def test_all_amenities(self):
 
-        response = self.client.get("/api/v1/rooms/amenities/")
+        response = self.client.get(self.URL)
 
         data = response.json()
 
@@ -21,3 +22,38 @@ class TestAmenities(APITestCase):
         self.assertEqual(len(data), 1)
         self.assertEqual(data[0]["name"], self.NAME)
         self.assertEqual(data[0]["description"], self.DESCRIPTION)
+
+    def test_create_amenity(self):
+
+        new_amenity_name = "New Amenity"
+        new_amenity_description = "New Amenity desc."
+
+        response = self.client.post(
+            self.URL,
+            data={
+                "name": new_amenity_name,
+                "description": new_amenity_description,
+            },
+        )
+
+        data = response.json()
+
+        self.assertEqual(
+            response.status_code,
+            200,
+            "Not 200 status code",
+        )
+        self.assertEqual(
+            data["name"],
+            new_amenity_name,
+        )
+        self.assertEqual(
+            data["description"],
+            new_amenity_description,
+        )
+
+        response = self.client.post(self.URL)
+        data = response.json()
+
+        self.assertEqual(response.status_code, 400)
+        self.assertIn("name", data)
