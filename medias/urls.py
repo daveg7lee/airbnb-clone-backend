6afcec3c2216a8bf22a095dd18f7f57b0eb3d8c5
daveg7lee <daveg7lee@gmail.com>
2022-10-27 00:01:48 +0900

    One Time Upload

--- medias/urls.py
+++ medias/urls.py
@@ -3,7 +3,11 @@ from . import views
 
 urlpatterns = [
     path(
+        "photos/get-url",
+        views.GetUploadURL.as_view(),
+    ),
+    path(
         "photos/<int:pk>",
         views.PhotoDetail.as_view(),
-    )
+    ),
 ]
--- medias/views.py
+++ medias/views.py
@@ -1,3 +1,5 @@
+import requests
+from django.conf import settings
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.exceptions import NotFound, PermissionDenied
@@ -27,3 +29,18 @@ class PhotoDetail(APIView):
         photo.delete()
 
         return Response(status=HTTP_204_NO_CONTENT)
+
+
+class GetUploadURL(APIView):
+    def post(self, request):
+        url = f"https://api.cloudflare.com/client/v4/accounts/{settings.CF_ID}/images/v2/direct_upload"
+
+        one_time_url = requests.post(
+            url,
+            headers={"Authorization": f"Bearer {settings.CF_TOKEN}"},
+        )
+
+        one_time_url = one_time_url.json()
+        result = one_time_url.get("result")
+
+        return Response({"uploadURL": result.get("uploadURL")})
